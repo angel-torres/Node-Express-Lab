731cce6e2e92f3ce497020af60dffbc5ba97a96f
Angel Torres <alas4224@gmail.com>
2019-02-12 22:07:46 -0600

    finished MVP

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -62,4 +62,25 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const post = req.body
+    if (!req.body.title | !req.body.contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    } else {
+        Posts
+        .update(id, post)
+        .then( bool => {
+            if (!bool) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else {
+                res.status(200).json({ message: "Post has been updated."})
+            }
+        })
+        .catch( err => {
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
+    }
+})
+
 module.exports = router;
